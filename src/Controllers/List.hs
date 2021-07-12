a8535a466e9fb99acfc256b53a6949e5ec98f9bd
Ranjit Jhala <jhala@cs.ucsd.edu>
2021-07-12 15:11:37 -0700

    v4 -- add list

---
+++ src/Controllers/List.hs
@@ -0,0 +1,49 @@
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+{-@ LIQUID "--no-pattern-inline" @-}
+
+module Controllers.List where
+
+import           Storm.Actions      -- LH: name resolution 
+import           Storm.Frankie (requireAuthUser,  status200 )
+import           Storm.SMTP        -- LH: name resolution bug 
+import           Control
+import           Model             -- LH: name resolution bug
+import           Storm.JSON (notFoundJSON, decodeBody, respondJSON)
+import           Storm.Filters
+import           Storm.Time
+import qualified Data.Text as T
+import           Storm.Infrastructure
+import           Control.Monad.Time (MonadTime(currentTime))
+import           Util (tShow)
+import           Types
+import           Storm.Insert (insert)
+import           Storm.Helpers
+
+------------------------------------------------------------------------------
+-- | Extract User Info List
+------------------------------------------------------------------------------
+{-@ list :: UserId -> TaggedT<{\_ -> False}, {\_ -> True}> _ _ _ @-}
+list :: UserId -> Controller ()
+list userId = do
+  viewerId  <- project userId' =<< requireAuthUser
+  let pub    = if userId == viewerId then trueF else itemLevel' ==. "public" 
+  items     <- selectList (itemOwner' ==. userId &&: pub)
+  itemDatas <- mapT (\i -> ItemData `fmap` project itemDescription' i 
+                                    <*>    project itemLevel' i) 
+                    items
+  respondJSON status200 itemDatas
+
+------------------------------------------------------------------------------
+-- | Add a new item for logged in user
+------------------------------------------------------------------------------
+{-@ add :: TaggedT<{\_ -> True}, {\_ -> True}> _ _ _ @-}
+add :: Controller ()
+add = do
+  owner   <- requireAuthUser
+  ownerId <- project userId' owner 
+  ownerEmail <- project userEmailAddress' owner
+  items   <- decodeBody
+  mapT (\ItemData {..} -> insert (mkItem ownerId itemDescription itemLevel)) items
+  respondJSON status200 ("OK: added " <> tShow (length items) <> " items for " <> ownerEmail)
