5ad83b123f58faa8c773b8b69099f74fa778366f
Ranjit Jhala <jhala@cs.ucsd.edu>
2021-07-12 15:21:17 -0700

    v1

---
+++ src/Controllers/List.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+{-@ LIQUID "--no-pattern-inline" @-}
+
+module Controllers.List where
+
+import           Storm.Actions      -- LH: name resolution 
+import           Storm.Frankie (requireAuthUser,  status200 )
+import           Storm.SMTP        -- LH: name resolution bug 
+import           Control
+import           Model             -- LH: name resolution bug
+import           Storm.JSON (notFoundJSON, decodeBody, respondJSON)
+import           Storm.Filters
+import           Storm.Time
+import qualified Data.Text as T
+import           Storm.Infrastructure
+import           Control.Monad.Time (MonadTime(currentTime))
+import           Util (tShow)
+
+------------------------------------------------------------------------------
+-- | template "ping-pong" respond
+------------------------------------------------------------------------------
+pong :: Controller ()
+pong = respondJSON status200 ("pong" :: T.Text)
+
+------------------------------------------------------------------------------
+-- | Extract User Info List
+------------------------------------------------------------------------------
+{-@ list :: UserId -> TaggedT<{\_ -> False}, {\_ -> True}> _ _ _ @-}
+list :: UserId -> Controller ()
+list uid = do 
+  time   <- getTime
+  respondJSON status200 ("OK: hello " <> tShow uid <> " at " <> time :: T.Text)
+
+getTime :: Controller T.Text
+getTime = tShow <$> liftTIO currentTime
