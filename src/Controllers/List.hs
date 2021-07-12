df306410d25a77bba9f62367382d8b00390cfd9a
Ranjit Jhala <jhala@cs.ucsd.edu>
2021-07-12 15:13:51 -0700

    v2 -- add list

---
+++ src/Controllers/List.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+{-@ LIQUID "--no-pattern-inline" @-}
+
+module Controllers.List where
+
+import           Storm.Actions      -- LH: name resolution 
+import           Storm.Frankie (requireAuthUser,  status200 )
+import           Storm.SMTP        -- LH: name resolution bug 
+import           Control
+import           Model             -- LH: name resolution bug
+import           Storm.JSON (notFoundJSON, decodeBody, respondJSON)
+import           Storm.Filters
+import           Storm.Time
+import qualified Data.Text as T
+import           Storm.Infrastructure
+import           Control.Monad.Time (MonadTime(currentTime))
+import           Util (tShow)
+import           Types
+import           Storm.Insert (insert)
+import           Storm.Helpers
+import Controllers.User (extractUserNG)
+
+------------------------------------------------------------------------------
+-- | Extract User Info List
+------------------------------------------------------------------------------
+{-@ list :: UserId -> TaggedT<{\_ -> False}, {\_ -> True}> _ _ _ @-}
+list :: UserId -> Controller ()
+list uid = do 
+  requireAuthUser
+  time   <- getTime
+  user   <- selectFirstOr notFoundJSON (userId' ==. uid)
+  userNG <- extractUserNG user
+  respondJSON status200 ("OK: hello " <> tShow userNG <> " at " <> time :: T.Text)
+
+getTime :: Controller T.Text
+getTime = tShow <$> liftTIO currentTime
